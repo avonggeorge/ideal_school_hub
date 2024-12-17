0127e961a7983d0ddb3af0bc098aee769216d94f
avonggeorge <georgeavong@yahoo.com>
2024-12-17 19:43:04 +0100

    feat(BigCalendar): Implement React Big Calendar component with event data

---
+++ src/components/BigCalender.tsx
@@ -0,0 +1,19 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import { calendarEvents } from '@/lib/data';
+
+const localizer = momentLocalizer(moment)
+
+const BigCalendar = () => (
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={calendarEvents}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+)
+
+export default BigCalendar;
