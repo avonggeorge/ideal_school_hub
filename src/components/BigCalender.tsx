ad2d557ce45aa8125433c0af50c57dab308b6d6d
avonggeorge <georgeavong@yahoo.com>
2024-12-18 17:12:52 +0100

    Added new line

--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import { calendarEvents } from '@/lib/data';
+import'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment)
 
@@ -12,6 +15,8 @@ const BigCalendar = () => (
       startAccessor="start"
       endAccessor="end"
       style={{ height: 500 }}
+      min={new Date(2025, 1,0,8,0,0)}
+      max={new Date(2025, 1,0,17,0,0)}
     />
   </div>
 )
