a3d609173e9bcd30a43520139fa04a9d3128bc1d
avonggeorge <georgeavong@yahoo.com>
2024-12-19 15:38:46 +0100

    feat(dashboard/list/teachers): Add search and filter functionality to TeacherListPage

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -4,13 +4,16 @@ const TeacherListPage = () => {
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
             {/* TOP */}
-            <div className="flex item-center justify-betweem">
+            <div className="flex item-center justify-between">
                 <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
                 <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
-                    <TableSearch />
+                    <div className="ml-auto">
+                        <TableSearch />
+                    </div>
                     <div className="">
                         <button>
-                            <span className="text-white font-semibold">Add Teacher</span>
+                            <span className="text-sm font-medium">Filter</span>
+                            <span className="ml-1 text-gray-400">By Department</span>
                         </button>
                     </div>
                 </div>
--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -3,7 +3,7 @@ import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons"; // Ensure the
 
 const TableSearch = () => {
     return (
-        <div className='hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2'>
+        <div className='w-full md:auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2'>
             <FontAwesomeIcon icon={faCalendarAlt} color="#ADD8E6" size="lg" />
             <input
                 type="text"
