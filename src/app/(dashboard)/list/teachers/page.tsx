1f8ce388e0b403cce0d4f389621d6b5e55c7a181
avonggeorge <georgeavong@yahoo.com>
2024-12-20 22:17:29 +0100

    Create table head columns and add buttons

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,6 +1,90 @@
+import Pagination from "@/components/Pagination";
 import TableSearch from "@/components/TableSearch";
+import Table from "@/components/Table";
+
+type Teacher = {
+    teacherID: string;
+    id: string;
+    name: string;
+    email: string;
+    photo: string;
+    subjects: string[];
+    classes: string[];
+    phone: string;
+    address: string;
+}
+
+const columns = [
+    {
+        header:"Info",
+        accessor:"info",
+    },
+    {
+        header: "Teacher ID",
+        accessor: "teacherID",
+        className: "hidden md:table-cell",
+    },
+    {
+        header: "Subjects",
+        accessor: "subjects",
+        className: "hidden md:table-cell",
+    },
+    {
+        header: "Classes",
+        accessor: "classes",
+        className: "hidden md:table-cell",
+    },
+    {
+        header: "Phone",
+        accessor: "phone",
+        className: "hidden lg:table-cell",
+    },
+    {
+        header: "Address",
+        accessor: "address",
+        className: "hidden lg:table-cell",
+    },
+    {
+        header: "Actions",
+        accessor: "action",
+    },
+];
 
 const TeacherListPage = () => {
+
+
+     const renderRow = (item:Teacher) => {
+        return (
+            <tr>
+                <td>
+                    {/* <Image src={item.photo} alt="pic" width={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover"/> */}
+                    <div className="flex flex-col">
+                        <h3 className="font-semibold">{item.name}</h3>
+                        <p className="text-xs text-gray-500">{item?.email}</p>
+                    </div>
+                </td>
+                <td> className="hidden md:table-cell"{item.teacherID}</td>
+                <td> className="hidden md:table-cell"{item.subjects.join(",")}</td>
+                <td> className="hidden md:table-cell"{item.classes.join(",")}</td>
+                <td> className="hidden md:table-cell"{item.phone}</td>
+                <td> className="hidden md:table-cell"{item.address}</td>
+                <td>
+                    <div className="flex items-center gap-2">
+                        <Link href={`/list/teachers/${item.id}`}>
+                            <button className="w-7 h-7 flex-center justify-center rounded-full bg-red">
+                                del
+                            </button>
+                        </Link>
+                        <button className="w-7 h-7 flex-center justify-center rounded-full bg-lamaSky">
+                                view
+                            </button>
+                    </div>
+                </td>
+            </tr>
+        )
+    };
+
+
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
             {/* TOP */}
@@ -10,20 +94,30 @@ const TeacherListPage = () => {
                     <div className="ml-auto">
                         <TableSearch />
                     </div>
-                    <div className="">
+                    <div className="w-8 h-8 flex items-center justify-center rounded-full ">
                         <button>
                             <span className="text-sm font-medium">Filter</span>
-                            <span className="ml-1 text-gray-400">By Department</span>
+                            {/* <span className="ml-1 text-gray-400">By Department</span> */}
+                        </button>
+                    </div>
+                    <div className="w-8 h-8 flex items-center justify-center rounded-full ">
+                        <button>
+                            <span className="text-sm font-medium">Sort</span>
+                        </button>
+                    </div>
+                    <div className="w-8 h-8 flex items-center justify-center rounded-full ">
+                        <button>
+                            <span className="text-sm font-medium">Add</span>
                         </button>
                     </div>
                 </div>
             </div>
             {/* LIST */}
-            <div className=""></div>
+            <Table columns={columns}/>
             {/* PAGINATION */}
-            <div className=""></div>
+            <Pagination />
         </div>
-    )
-}
+    );
+};
 
 export default TeacherListPage;
