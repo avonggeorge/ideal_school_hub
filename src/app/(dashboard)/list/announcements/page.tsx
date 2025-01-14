ec112251bb5ec5b49c4438a42200aec628e9d6db
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2025-01-14 10:18:01 +0100

    feat(announcements): implement dynamic filtering and pagination for announcements
    - Added search parameter support to filter announcements based on title with case insensitivity.
    - Implemented role-based conditions for teachers, students, and parents to display relevant announcements.
    - Included pagination logic using  query parameter and  constant.
    - Integrated Prisma transactions to fetch announcements with associated class details and total count in a single operation.

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,17 +1,26 @@
-import FormModal from "@/components/FormModal";
+import FormContainer from "@/components/FormContainer";
 import Pagination from "@/components/Pagination";
 import TableSearch from "@/components/TableSearch";
 import Table from "@/components/Table";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSort, faFilter } from '@fortawesome/free-solid-svg-icons';
-import { announcementsData, role } from "@/lib/data";
+import prisma from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Announcement, Class, Prisma } from "@prisma/client";
+import { auth } from "@clerk/nextjs/server";
 
-type Announcement = {
-    id: number;
-    title: string;
-    class: string;
-    date: string;
-};
+
+type AnnouncementList = Announcement & { class: Class };
+const AnnouncementListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  
+  const { userId, sessionClaims } = await auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const currentUserId = userId;
+  
 
 const columns = [
     {
@@ -28,30 +37,85 @@ const columns = [
         className: "hidden md:table-cell",
     },
 
-    {
-        header: "Actions",
-        accessor: "action",
-    },
+    ...(role === "admin"
+      ? [
+          {
+            header: "Actions",
+            accessor: "action",
+          },
+        ]
+      : []),
 ];
 
-const AnnouncementListPage = () => { 
-   const renderRow = (item: Announcement) => (
+
+   const renderRow = (item: AnnouncementList) => (
             <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-colorMintGreen">
                 <td className="flex items-center gap-4 p-4">{item.title}</td>
-                <td>{item.class}</td>
-                <td className="hidden md:table-cell">{item.date}</td>
+                <td>{item.class?.name || "-"}</td>
+                <td className="hidden md:table-cell">{new Intl.DateTimeFormat("en-US").format(item.date)}
+      </td>
                 <td>
                     <div className="flex items-center gap-2">
                         {role === "admin" && (
             <>
-              <FormModal table="announcement" type="update" data={item} />
-              <FormModal table="announcement" type="delete" id={item.id} />
+               <FormContainer table="announcement" type="update" data={item} />
+              <FormContainer table="announcement" type="delete" id={item.id} />
             </>
           )}
         </div>
       </td>
     </tr>
   );
+
+   const { page, ...queryParams } = searchParams;
+
+  const p = page ? parseInt(page) : 1;
+
+  // URL PARAMS CONDITION
+
+  const query: Prisma.AnnouncementWhereInput = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "search":
+            query.title = { contains: value, mode: "insensitive" };
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  // ROLE CONDITIONS
+
+  const roleConditions = {
+    teacher: { lessons: { some: { teacherId: currentUserId! } } },
+    student: { students: { some: { id: currentUserId! } } },
+    parent: { students: { some: { parentId: currentUserId! } } },
+  };
+
+  query.OR = [
+    { classId: null },
+    {
+      class: roleConditions[role as keyof typeof roleConditions] || {},
+    },
+  ];
+
+  const [data, count] = await prisma.$transaction([
+    prisma.announcement.findMany({
+      where: query,
+      include: {
+        class: true,
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.announcement.count({ where: query }),
+  ]);
+
 
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -69,15 +133,15 @@ items-center justify-between">
              <FontAwesomeIcon icon={faSort} style={{ width: '14px', height: '14px' }} />
             </button>
             {role === "admin" && (
-              <FormModal table="announcement" type="create" />
+               <FormContainer table="announcement" type="create" />
             )}
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={announcementsData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
       {/* PAGINATION */}
-      <Pagination />
+      <Pagination page={p} count={count} />
     </div>
   );
 };
