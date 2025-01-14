d2402adbf6c855dc6db5510315e8a5629dab041c
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2025-01-14 10:39:46 +0100

    feat(parents): add dynamic search and pagination for parent records
    - Implemented search functionality for parents based on their name with case-insensitive matching.
    - Added pagination to efficiently handle large sets of parent records using  and .
    - Included related student records for each parent in the Prisma query.
    - Used Prisma transactions to fetch parent data along with associated students in an optimized manner.

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -1,70 +1,120 @@
 import Pagination from "@/components/Pagination";
 import TableSearch from "@/components/TableSearch";
 import Table from "@/components/Table";
-import FormModal from "@/components/FormModal";
+import FormContainer from "@/components/FormContainer";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilter, faSort } from '@fortawesome/free-solid-svg-icons';
-import { parentsData, role } from "@/lib/data";
+import prisma from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Parent, Prisma, Student } from "@prisma/client";
+import Image from "next/image";
 
-type Parent = {
-    id: number;
-    name: string;
-    students: string[];
-    email?: string;
-    phone: string;
-    address: string;
-};
+import { auth } from "@clerk/nextjs/server";
+
+type ParentList = Parent & { students: Student[] };
+
+const ParentListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+
+const { sessionClaims } = await auth();
+const role = (sessionClaims?.metadata as { role?: string })?.role;
+
 
 const columns = [
-    {
-        header: "Info",
-        accessor: "info",
-    },
-    {
-        header: "Student Names",
-        accessor: "students",
-        className: "hidden md:table-cell",
-    },
-    {
-        header: "Phone",
-        accessor: "phone",
-        className: "hidden lg:table-cell",
-    },
-    {
-        header: "Address",
-        accessor: "address",
-        className: "hidden lg:table-cell",
-    },
-    {
-        header: "Actions",
-        accessor: "action",
-    },
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "Student Names",
+    accessor: "students",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell",
+  },
+  ...(role === "admin"
+    ? [
+        {
+          header: "Actions",
+          accessor: "action",
+        },
+      ]
+    : []),
 ];
 
-const ParentListPage = () => {
-      const renderRow = (item: Parent) => (
-            <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-colorMintGreen">
-                <td>
-                    <div className="flex flex-col">
-                        <h3 className="font-semibold">{item.name}</h3>
-                        <p className="text-xs text-gray-500">{item.email}</p>
-                    </div>
-                </td>
-                <td className="hidden md:table-cell">{item.students.join(",")}</td>
-                <td className="hidden md:table-cell">{item.phone}</td>
-                <td className="hidden lg:table-cell">{item.address}</td>
-                <td>
-                    <div className="flex items-center gap-2">
-                        {role === "admin" && (
-            <>
-              <FormModal table="parent" type="update" data={item} />
-              <FormModal table="parent" type="delete" id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+const renderRow = (item: ParentList) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">
+      <div className="flex flex-col">
+        <h3 className="font-semibold">{item.name}</h3>
+        <p className="text-xs text-gray-500">{item?.email}</p>
+      </div>
+    </td>
+    <td className="hidden md:table-cell">
+      {item.students.map((student) => student.name).join(",")}
+    </td>
+    <td className="hidden md:table-cell">{item.phone}</td>
+    <td className="hidden md:table-cell">{item.address}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          <>
+            <FormContainer table="parent" type="update" data={item} />
+            <FormContainer table="parent" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
+
+  const { page, ...queryParams } = searchParams;
+
+  const p = page ? parseInt(page) : 1;
+
+  // URL PARAMS CONDITION
+
+  const query: Prisma.ParentWhereInput = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "search":
+            query.name = { contains: value, mode: "insensitive" };
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  const [data, count] = await prisma.$transaction([
+    prisma.parent.findMany({
+      where: query,
+      include: {
+        students: true,
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.parent.count({ where: query }),
+  ]);
 
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -80,16 +130,15 @@ const ParentListPage = () => {
                     <button className="w-8 h-8 flex items-center justify-center rounded-full bg-colorBlue">
                             <FontAwesomeIcon icon={faSort} style={{ width: '14px', height: '14px' }} />
                         </button>
-                    {role === "admin" && (
-              <FormModal table="teacher" type="create"/>
-            )}
+                     {role === "admin" && <FormContainer table="parent" type="create" />
+                     }
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={parentsData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
       {/* PAGINATION */}
-      <Pagination />
+      <Pagination page={p} count={count} />
     </div>
   );
 };
