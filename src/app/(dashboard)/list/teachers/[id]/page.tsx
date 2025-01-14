2c530a50d1c5b31d0d0c33f2162a78fe6033cf02
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2025-01-14 10:49:57 +0100

    feat(calendar): Integrate BigCalendar for teacher-specific view
    - Added `BigCalendarContainer` component to display calendar for a specific teacher.
    - Passed teacher ID (`teacher.id`) and type (`teacherId`) to filter calendar events by teacher.

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,15 +1,13 @@
 import Announcements from "@/components/Announcements";
-import BigCalendar from "@/components/BigCalendar";
-// import BigCalendarContainer from "@/components/BigCalendarContainer";
+import BigCalendarContainer from "@/components/BigCalendarContainer";
 import FormContainer from "@/components/FormContainer";
 import Performance from "@/components/Performance";
-import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faBuilding, faCalendarAlt, faFilter, faPhone, faTint, faEnvelope, faChalkboard, faUserCheck } from '@fortawesome/free-solid-svg-icons';
 import prisma from "@/lib/prisma";
-// import { auth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { Teacher } from "@prisma/client";
 import { notFound } from "next/navigation";
 
@@ -18,8 +16,8 @@ const SingleTeacherPage = async ({
 }: {
   params: { id: string };
 }) => {
-  // const { sessionClaims } = auth();
-  // const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const { sessionClaims } = await auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
 
   const teacher:
     | (Teacher & {
@@ -83,7 +81,7 @@ const SingleTeacherPage = async ({
                             </div>
                             <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                 <FontAwesomeIcon icon={faCalendarAlt} style={{ width: '14px', height: '14px', color: 'gray' }} />
-                                <span>{new Intl.DateTimeFormat("en-GB").format(teacher.birthday)}</span>
+                                <span>{new Intl.DateTimeFormat("en-GB").format(teacher.birthdate)}</span>
                             </div>
                             <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                 <FontAwesomeIcon icon={faEnvelope} style={{ width: '14px', height: '14px', color: 'blue' }} />
@@ -135,7 +133,7 @@ const SingleTeacherPage = async ({
         {/* BOTTOM */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
           <h1>Teacher&apos;s Schedule</h1>
-           {/* <BigCalendarContainer type="teacherId" id={teacher.id} /> */}
+           <BigCalendarContainer type="teacherId" id={teacher.id} />
         </div>
       </div>
       {/* RIGHT */}
