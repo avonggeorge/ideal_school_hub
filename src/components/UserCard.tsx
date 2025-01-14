f420f47e6a6c8f931b8903b6217bd08929abc755
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2025-01-14 17:15:34 +0100

    "Add UserCard component to dynamically count and display user types (admin, teacher, student, parent)"

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,9 +2,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEllipsis
 } from "@fortawesome/free-solid-svg-icons";
+import prisma from "@/lib/prisma";
 
 
-const UserCard = ({ type }: { type: string }) => {
+const UserCard = async ({
+  type,
+}: {
+  type: "admin" | "teacher" | "student" | "parent";
+}) => {
+  const modelMap: Record<typeof type, any> = {
+    admin: prisma.admin,
+    teacher: prisma.teacher,
+    student: prisma.student,
+    parent: prisma.parent,
+  };
+
+  const data = await modelMap[type].count();
    return (
     <div className="rounded-2xl odd:bg-colorMintGreen even:bg-colorBlue p-4 flex-1 min-w-[130px]">
         <div className="flex justify-between items-center">
@@ -17,7 +30,7 @@ const UserCard = ({ type }: { type: string }) => {
         />
 
       </div>
-      <h1 className="text-2xl font-semibold my-4">1,234</h1>
+      <h1 className="text-2xl font-semibold my-4">{data}</h1>
       <h2 className="capitalize text-sm font-medium text-slate-600">{type}s</h2>
      </div>
    );
